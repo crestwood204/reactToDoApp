4f78b4d480725322f8339cca1f9b4d138c132865
crestwood204 <andong@sas.upenn.edu>
2017-06-30 14:11:57 -0700

    added css file to build and linked it to html

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,45 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const dummyData = ['play lol', 'update servers', 'code all day', 'talk to court', 'call home'];
-// ReactDOM.render(<p>I'm Changing!</p>,
-//    document.getElementById('root'));
-//app.use(express.static(path.join(__dirname, 'public')));
+//import components
+import ToDoApp from './components/ToDoApp';
 
-class ToDo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <li>
-          <button>
-            X
-          </button>
-          {this.props.todo}
-        </li>
-      </div>
-    )
-  }
-}
-
-class ToDoList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    console.log(dummyData);
-    return (
-      <div>
-        <ul>
-          {dummyData.map((todo) => (<ToDo key={todo} todo={todo} />))}
-        </ul>
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<ToDoList />, document.getElementById('root'));
+ReactDOM.render(<ToDoApp />, document.getElementById('root'));
